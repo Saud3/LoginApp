0491a2e09bc53f0e37a8aab44ed4e325dba4b18e
Saud3 <saud.3h@gmail.com>
2021-09-26 01:09:31 +0300

    Login app

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       var emailText = findViewById<TextView>(R.id.ptEmailText)
-       var passworText = findViewById<TextView>(R.id.ptPasswordText)
+       val emailText = findViewById<TextView>(R.id.userName)
+       val passwordText = findViewById<TextView>(R.id.ptPasswordText)
+        val loginButton = findViewById<Button>(R.id.butLogin)
+       // val signupButton = findViewById<Button>(R.id.btnSignup)
+
+
+        loginButton.setOnClickListener {
+            //if ((emailText.text=="a") && (passwordText.text== "1")){
+                val intent = Intent(this,SignUpActivity::class.java)
+                startActivity(intent)
+
+        //}
+           // else{
+            //    Toast.makeText(this,"Invalid Input",Toast.LENGTH_LONG).show()
+           //}
+        }
 
     }
 }
